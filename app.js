4593cbe7de86a25a12ddbdf952f3000078a778b4
gigu007 <gideonukaegbu007@gmail.com>
2022-04-07 11:05:11 +0100

    another commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //Select the content elements from the DOM
-const questions = document.querySelectorAll('.label');
+const FAQs = document.querySelectorAll('.icon');
 
 //Loop through all the elements and add an event listener to them
-questions.forEach((question) =>
+FAQs.forEach((question) =>
   question.addEventListener('click', () => {
     if (question.parentNode.classList.contains('active')) {
       question.parentNode.clasList.toggle('active');
     } else {
-      questions.forEach((question) => question.parentNode
+      FAQs.forEach((question) => question.parentNode
       .classList.remove('active'));
       question.parentNode.classList.add('active');
     }
